8b6bdd225317efc28b0cc2721cd59490af0e6a2f
hemiye <hemiyecha@gmail.com>
2024-09-14 15:05:12 +0300

    todo UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:todoey_flutter/screens/tasks_screen.dart';
-import 'package:provider/provider.dart';
-import 'package:todoey_flutter/models/task_data.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      builder: (context) => TaskData(),
-      child: MaterialApp(
-        home: TasksScreen(),
-      ),
+    return MaterialApp(
+      home: TasksScreen(),
     );
   }
 }
